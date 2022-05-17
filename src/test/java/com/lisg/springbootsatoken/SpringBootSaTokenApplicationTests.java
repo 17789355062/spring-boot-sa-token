2ebf094a2207ee24155c9ebd2df85113776aa614
lisg <lishiguang_wx>
2022-05-17 10:27:49 +0800

    初始化

---
+++ src/test/java/com/lisg/springbootsatoken/SpringBootSaTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisg.springbootsatoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSaTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
